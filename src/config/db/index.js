12bec3ab1eacc7058b32ce87ff0bc64701add8f7
phuhnf8 <phuhnf8@fullstack.edu.vn>
2026-02-03 23:51:17 +0700

    hotfix: missing ssl authentication for db connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const { DB_HOST, DB_USER, DB_NAME, DB_PASS, DB_PORT } = process.env;
-const dbInterface = mysql.createPool({
+const fs = require("fs");
+const { join } = require("path");
+
+const dbConfig = {
 	host: DB_HOST,
 	user: DB_USER,
 	password: DB_PASS,
@@ -9,11 +12,20 @@ const dbInterface = mysql.createPool({
 	port: DB_PORT ? Number.parseInt(DB_PORT) : 3306,
 	waitForConnections: true,
 	connectionLimit: 10,
-	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	maxIdle: 10,
+	idleTimeout: 60000,
 	queueLimit: 0,
 	enableKeepAlive: true,
 	keepAliveInitialDelay: 0
-});
+};
+
+if (process.env.NODE_ENV === "production") {
+	const path = join(__dirname, "ca.pem");
+	dbConfig.ssl = {
+		ca: fs.readFileSync(path)
+	};
+}
+
+const dbInterface = mysql.createPool(dbConfig);
 
 module.exports = dbInterface;
