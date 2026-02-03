d61dacef76a6011f614d7c2d9cf59dd58f4bc8dc
phuhnf8 <phuhnf8@fullstack.edu.vn>
2026-02-03 23:07:06 +0700

    chore: final commit for assignment 6

--- db/sql.sql
+++ db/sql.sql
@@ -19,58 +19,52 @@
 CREATE DATABASE IF NOT EXISTS `db_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `db_test`;
 
--- Dumping structure for table db_test.conversation_participants
-CREATE TABLE IF NOT EXISTS `conversation_participants` (
-  `conversation_id` int unsigned NOT NULL,
-  `user_id` int unsigned NOT NULL,
-  PRIMARY KEY (`conversation_id`,`user_id`),
-  KEY `FK_conversation_participant_user_id` (`user_id`),
-  CONSTRAINT `FK_conversation_participant_conversation_id` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_conversation_participant_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+-- Dumping structure for table db_test.revoked_tokens
+CREATE TABLE IF NOT EXISTS `revoked_tokens` (
+  `access_token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `out_at` timestamp NOT NULL DEFAULT (now()),
+  PRIMARY KEY (`access_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Track logout history';
+
+-- Dumping data for table db_test.revoked_tokens: ~0 rows (approximately)
+INSERT INTO `revoked_tokens` (`access_token`, `user_id`, `out_at`) VALUES
+	('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiZTFhMDY0Zi00ODUyLTRhMTgtOTBmZi0xYmJmZjYwZjQ5Y2UiLCJleHAiOjE3NzAxMTI4MDI5NjV9.KsCxpxSSW1jF793zKAn9O_ONDGoiexr1y9lE8RnLfio', 'be1a064f-4852-4a18-90ff-1bbff60f49ce', '2026-02-03 09:00:54'),
+	('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiZTFhMDY0Zi00ODUyLTRhMTgtOTBmZi0xYmJmZjYwZjQ5Y2UiLCJleHAiOjE3NzAxMTUzNzQzMzB9.9Wxmfo_C_PMVpKCjzJjblYNS-Hp_EpaNV_hzHidFbyI', 'be1a064f-4852-4a18-90ff-1bbff60f49ce', '2026-02-03 09:43:53');
+
+-- Dumping structure for table db_test.todos
+CREATE TABLE IF NOT EXISTS `todos` (
+  `id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Todo Id',
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Todo name',
+  `created_by` varchar(36) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'From user',
+  `created_at` timestamp NOT NULL DEFAULT (now()),
+  `status` enum('pending','done') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`id`),
+  KEY `FK_todos_created_by` (`created_by`),
+  CONSTRAINT `FK_todos_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
-
--- Dumping structure for table db_test.conversations
-CREATE TABLE IF NOT EXISTS `conversations` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `type` enum('group','direct') COLLATE utf8mb4_general_ci NOT NULL,
-  `created_by` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_conversations_created_by` (`created_by`),
-  CONSTRAINT `FK_conversations_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table db_test.messages
-CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `conversation_id` int unsigned DEFAULT NULL,
-  `sender_id` int unsigned DEFAULT NULL,
-  `content` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT (now()),
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `FK_messages_sender_id` (`sender_id`),
-  KEY `FK_messages_conversation_participants` (`conversation_id`,`sender_id`),
-  CONSTRAINT `FK_messages_conversation_id` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_messages_conversation_participants` FOREIGN KEY (`conversation_id`, `sender_id`) REFERENCES `conversation_participants` (`conversation_id`, `user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `FK_messages_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data exporting was unselected.
+-- Dumping data for table db_test.todos: ~0 rows (approximately)
+INSERT INTO `todos` (`id`, `title`, `created_by`, `created_at`, `status`) VALUES
+	('628d72f3-1a8c-4b34-93a5-fdce13a03749', 'Eat oranges', 'be1a064f-4852-4a18-90ff-1bbff60f49ce', '2026-02-03 09:43:48', 'pending'),
+	('6829a3d3-2394-41be-b863-74606729e04a', 'Do laundry', 'be1a064f-4852-4a18-90ff-1bbff60f49ce', '2026-02-03 09:43:33', 'pending'),
+	('751f58c8-9f62-4633-a636-31c3a85db01a', 'New Todo', 'be1a064f-4852-4a18-90ff-1bbff60f49ce', '2026-02-03 09:43:21', 'pending');
 
 -- Dumping structure for table db_test.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'UUID username',
+  `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `refresh_token` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'Refresh token strategy',
+  `refresh_token_ttl` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table db_test.users: ~1 rows (approximately)
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `refresh_token`, `refresh_token_ttl`) VALUES
+	('be1a064f-4852-4a18-90ff-1bbff60f49ce', 'phuhnf8', 'email@example.com', '$2b$10$PaFevb2KC9iWM8/WX330wO5bij0.K5dpNQEulzIJyYvOlLT/TOOVe', 'Mcjgndez4K53UMy0z4pF4Oqlnv4taywd', '2026-02-10 10:00:11');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
-# Express JS Day 05
+# Express JS Day 06
 
 # Overview
 
-This repository contains the code and the sql file to create chat application, using JWT based authentication system with Express JS and MySQL database.
+This repository contains the code and the sql file to create todo list application, using JWT based authentication system with Express JS and MySQL database.
 
 # Installation
 
@@ -31,25 +31,51 @@ Default server runs at `http://localhost:3000`
 
 # API Endpoints
 
-`POST /api/auth/register` - Signup new user
+`POST /auth/register` - Signup new user
 
-`POST /api/auth/login` - Login with email and password, receive JWT token
+```js
+{
+    username: "Your Name",
+    email: "email@example.com",
+    password: "your_password"
+}
+```
 
-`POST /api/conversations` - Create new conversation
+`POST /auth/login` - Login with email and password, receive JWT token
 
-`GET /api/conversations` - Get all conversations of the logged in user
+```js
+{
+    email: "email@example.com",
+    password: "your_password"
+}
+```
 
-`POST /api/conversations/:id/participants` - Add participant to conversation by conversation id and user_id in body
+`POST /auth/logout` - Logout user by invalidating the token
 
-`POST /api/conversations/:id/messages` - Send new message to the conversation
+`POST /auth/refresh-token` - Refresh JWT token with body payload:
 
-`GET /api/conversations/:id/messages` - Get messages from the conversation
+```js
+{
+	refresh_token: "your_refresh_token";
+	access_token: "your_access_token";
+}
+```
 
-`GET /api/users/search?q=email` - Search user by email to add to conversation
+Todos API (Requires Authorization header with Bearer token)
+`GET /api/todos` - Get all todos for the authenticated user
+`GET /api/todos/:id` - Get a specific todo by ID for the authenticated user
+`POST /api/todos` - Create a new todo
+
+```js
+{
+	title: "Todo Title";
+}
+```
+
+`DELETE /api/todos/:id` - Delete a specific todo by ID for the authenticated user
 
 # Todo List
 
 - [ ] Create Offset-based Pagination for Messages in Conversations
-- [ ] Hashing password before storing in database
+- [x] Hashing password before storing in database
 - [ ] Input validation, using Joi.
-- [ ] Alter data type of message_id
